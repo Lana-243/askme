1d7f6f30e886702cc5d397aee7984af328e78f23
Lana-243 <84347249+Lana-243@users.noreply.github.com>
2022-05-17 13:07:58 +0300

    added partials, helpers

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,19 +4,19 @@ class QuestionsController < ApplicationController
   def create
     question = Question.create(question_params)
 
-    redirect_to question_path(question)
+    redirect_to question_path(question), notice: "New question has been created!"
   end
 
   def update
     @question.update(question_params)
 
-    redirect_to question_path(@question)
+    redirect_to question_path(@question), notice: "Question has been saved"
   end
 
   def destroy
     @question.destroy
 
-    redirect_to questions_path
+    redirect_to questions_path, notice: "Question has been deleted"
   end
 
   def show
@@ -24,6 +24,7 @@ class QuestionsController < ApplicationController
   end
 
   def index
+    @question = Question.new
     @questions = Question.all
   end
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def inclination(count, one, many)
+    (count > 1) ? (return many) : (return one)
+  end
 end
