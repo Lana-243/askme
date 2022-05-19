100af3cfda80af35ddef0c37b23de293f2de7b62
Lana-243 <84347249+Lana-243@users.noreply.github.com>
2022-05-19 17:35:58 +0300

    added user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  # post '/questions', to: 'questions#create'
-  # patch '/questions/:id', to: 'questions#update'
-  # delete '/questions/:id', to: 'questions#destroy'
   root to: 'questions#index'
 
   resources :questions
+  resource :session, only: %i[new create destroy]
   resources :users, only: %i[new create]
 end
