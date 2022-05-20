e6da01d3457e6072853b758fd5ac681671b87736
Lana-243 <84347249+Lana-243@users.noreply.github.com>
2022-05-20 18:53:17 +0300

    added user update, extra features to questions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,10 +5,46 @@ class UsersController < ApplicationController
   end
 
   def create
-    user_params = params.require(:user).permit(:name, :nickname, :email, :password)
+    @user = User.create(user_params)
 
-    User.create(user_params)
+    if @user.save
+      redirect_to root_path, notice: "You have successfully registered"
+    else
+      flash.now[:alert] = "You have incorrectly filled in the form"
 
-    redirect_to root_path, notice: "You have successfully registered!"
+      render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to root_path, notice: "Your data has been updated"
+    else
+      flash.now[:alert] = "Please check the data"
+
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+
+    session.delete(:user_id)
+
+    redirect_to root_path, notice: 'User has been destroyed'
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :name, :nickname, :email, :password, :password_confirmation
+    )
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
 
   resources :questions
   resource :session, only: %i[new create destroy]
-  resources :users, only: %i[new create]
+  resources :users, only: %i[new create edit update destroy]
+
 end
