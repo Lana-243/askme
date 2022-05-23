1f5427de64b5f38acc7fde3fab9c304554a2aa92
Lana-243 <84347249+Lana-243@users.noreply.github.com>
2022-05-23 15:12:29 +0300

    added session upon registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,6 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-    session[:user_id] = @user.id
-
     if @user.save
       session[:user_id] = @user.id
 
