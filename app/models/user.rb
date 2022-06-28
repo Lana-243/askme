a3d5a5b05dcf38287e22d285a58702e5d0fc2132
Lana-243 <84347249+Lana-243@users.noreply.github.com>
2022-06-28 13:07:33 +0300

    added validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,17 @@
 class User < ApplicationRecord
   has_secure_password
 
-  before_validation :downcase_nickname
+  COLOR_FORMAT = /\A#\h{3}{1,2}\z/
+
+
+    before_validation :downcase_nickname
 
   validates :name, presence: true
   validates :nickname, presence: true, uniqueness: true,
             length: {maximum: 40}, format: { with: /\A[a-z_0-9]+\z/ }
   validates :email, presence: true, uniqueness: true,
             format: { with: /\A[a-z\d_+.\-]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i }
+  validates :header_color, format: { with: COLOR_FORMAT }
 
   has_many :questions, dependent: :delete_all
 
