212412c80d7e0d393ff1d38b05e4b94720af4a54
Lana-243 <84347249+Lana-243@users.noreply.github.com>
2022-06-29 18:04:09 +0300

    changed recaptcha method

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -13,7 +13,7 @@ class QuestionsController < ApplicationController
 
     @question.author = current_user
 
-    if verify_recaptcha(@question) && @question.save
+    if check_captcha(@question) && @question.save
       redirect_to user_path(@question.user), notice: "New question has been created!"
     else
       flash.now[:alert] = "You have incorrectly filled in the form"
