bdaa4ed26e13716b44932e0ccfbb71cf239775bb
Lana-243 <84347249+Lana-243@users.noreply.github.com>
2022-07-04 12:12:10 +0300

    changed line order

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
 
   COLOR_FORMAT = /\A#\h{3}{1,2}\z/
 
-
-  before_validation :downcase_nickname
+  has_many :questions, dependent: :delete_all
+  has_many :created_questions, class_name: "Question", foreign_key: "author_id"
 
   validates :name, presence: true
   validates :nickname, presence: true, uniqueness: true,
@@ -13,11 +13,12 @@ class User < ApplicationRecord
             format: { with: /\A[a-z\d_+.\-]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i }
   validates :header_color, format: { with: COLOR_FORMAT }
 
-  has_many :questions, dependent: :delete_all
-  has_many :created_questions, class_name: "Question", foreign_key: "author_id"
+  before_validation :downcase_nickname
 
   include Gravtastic
   gravtastic(secure: true, filetype: :png, size: 100, default: 'retro')
+
+  private
 
   def downcase_nickname
     nickname&.downcase!
