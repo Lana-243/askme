9d9b952bfa5b932a70bee4e0564b498c1f8d94c5
Lana-243 <84347249+Lana-243@users.noreply.github.com>
2022-07-06 11:24:53 +0300

    updated spaces in hashed, added a file to gitignore

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  has_secure_password
+  include Gravtastic
+  gravtastic(secure: true, filetype: :png, size: 100, default: 'retro')
 
   COLOR_FORMAT = /\A#\h{3}{1,2}\z/
 
@@ -8,15 +9,14 @@ class User < ApplicationRecord
 
   validates :name, presence: true
   validates :nickname, presence: true, uniqueness: true,
-            length: {maximum: 40}, format: { with: /\A[a-z_0-9]+\z/ }
+            length: { maximum: 40 }, format: { with: /\A[a-z_0-9]+\z/ }
   validates :email, presence: true, uniqueness: true,
             format: { with: /\A[a-z\d_+.\-]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i }
   validates :header_color, format: { with: COLOR_FORMAT }
 
   before_validation :downcase_nickname
 
-  include Gravtastic
-  gravtastic(secure: true, filetype: :png, size: 100, default: 'retro')
+  has_secure_password
 
   def to_param
     nickname
