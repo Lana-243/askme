9a4f7b54629a9cbd8ce5a19f859867cc718e4e1b
Lana-243 <84347249+Lana-243@users.noreply.github.com>
2022-07-27 21:12:07 +0300

    removed action that is not needed

--- app/controllers/hashtags_controller.rb
+++ app/controllers/hashtags_controller.rb
@@ -1,8 +1,4 @@
 class HashtagsController < ApplicationController
-  def create(params)
-    @text = params[:text]
-  end
-
   def show
     @hashtag = Hashtag.with_questions.find_by(text: params[:text])
     @questions = @hashtag.questions
