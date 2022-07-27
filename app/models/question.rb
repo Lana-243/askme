05ad73b7066fe36419accb67ef970550f51b92d6
Lana-243 <84347249+Lana-243@users.noreply.github.com>
2022-07-27 21:11:31 +0300

    removed dependent:destroy

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,7 @@
 class Question < ApplicationRecord
   belongs_to :user
   belongs_to :author, class_name: "User", optional: true
-  has_and_belongs_to_many :hashtags, dependent: :destroy
+  has_and_belongs_to_many :hashtags
 
   validates :body, presence: true, length: { maximum: 280 }
 
