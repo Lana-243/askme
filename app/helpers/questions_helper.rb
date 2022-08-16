e1128dae9393f078e2603eff63d24083b9daf86b
Lana-243 <84347249+Lana-243@users.noreply.github.com>
2022-08-16 17:14:17 +0300

    remove html_safe

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,5 +1,5 @@
 module QuestionsHelper
   def render_with_hashtags(body)
-    body.gsub(Hashtag::HASHTAG_FORMAT) { |word| link_to word, hashtag_path(word.downcase.delete('#'))}.html_safe
+    body.gsub(Hashtag::HASHTAG_FORMAT) { |word| link_to word, hashtag_path(word.downcase.delete('#'))}
   end
 end
