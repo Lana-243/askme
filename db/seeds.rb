a1e606da3a9d663ae316a40ee75c6b4b6edf033a
Lana-243 <84347249+Lana-243@users.noreply.github.com>
2022-09-28 22:07:24 +0300

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create!([
+                                        { name: "Mike", nickname: 'M', email: 'hey@gmail.com', password: '123' },
+                                        { name: "Delon", nickname: 'T', email: 'h1@gmail.com', password: '1234' },
+                                        { name: "Martha", nickname: 'M12', email: 'h2@gmail.com', password: '12356' }
+                                      ])
